95c6a78444db91539b53530ebf3511e199fb4884
Shreyash Chavhan <shreyashchavhan@Shreyashs-MacBook-Air.local>
2024-01-27 14:42:53 +0530

    prefix to postfix

---
+++ must-topics/stack/infix_prefix_postfix/prefix_to_postfix.py
@@ -0,0 +1,34 @@
+from queue import deque
+
+def prec(c):
+    if c == '^':
+        return 3
+    elif c == '*' or c == '/':
+        return 2
+    elif c == '+' or c == '-':
+        return 1
+    else:
+        return 0
+
+def isOperand(c):
+    if (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or (c >= '0' and  c <= '9'):
+        return 1 
+    return 0
+
+def prefix_to_postfix(s):
+    s = s[::-1]
+    stack = deque()
+
+    for c in s:
+        if isOperand(c):
+            stack.append(c)
+        else:
+            first = stack.pop()
+            second = stack.pop()
+            curr = first + second + c 
+            stack.append(curr)
+    return stack.pop() 
+
+# s = "-*+ABCD"
+s = "-+ABC"
+print(prefix_to_postfix(s))
