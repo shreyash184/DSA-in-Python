e76f522c4905d962e6c046b329af2bd32bfe5ee1
Shreyash Chavhan <shreyashchavhan@Shreyashs-MacBook-Air.local>
2024-01-27 15:53:30 +0530

    postfix to prefix added

---
+++ must-topics/stack/infix_prefix_postfix/postfix_to_prefix.py
@@ -0,0 +1,24 @@
+from queue import deque
+
+def isOperand(c):
+    if (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or (c >= '0' and  c <= '9'):
+        return 1 
+    return 0
+
+def postfix_to_prefix(s):
+    stack = deque()
+
+    for c in s:
+        if isOperand(c):
+            stack.append(c)
+        else:
+            op2 = stack.pop()
+            op1 = stack.pop()
+            curr = c + op1 + op2
+            stack.append(curr)
+
+    return stack.pop()
+
+# s = "AB+CD-*"
+s = "ABC/-AK/L-*"
+print(postfix_to_prefix(s))
